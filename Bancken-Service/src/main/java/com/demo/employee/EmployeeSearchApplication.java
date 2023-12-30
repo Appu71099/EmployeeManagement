d9abb6ca2ebe9e90d55e4f1f02ad03f90bdc2419
Prashik <Pramteke223@gmail.com>
2023-12-30 11:52:24 +0530

    My first commit

--- Bancken-Service/src/main/java/com/demo/employee/EmployeeSearchApplication.java
+++ Bancken-Service/src/main/java/com/demo/employee/EmployeeSearchApplication.java
@@ -7,7 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeSearchApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeeSearchApplication.class, args);
+		SpringApplication.run(EmployeeSearchApplication.class, args); 
+		//I have write some code
 	}
 
 }
